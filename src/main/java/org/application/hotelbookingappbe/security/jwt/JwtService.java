f008ab941e91597a8948b7ef14d3255df349d8db
Ahmet Sami Yeniçeri <ahmet.sami.yeniceri@ericsson.com>
2024-11-26 15:10:28 +0300

    JWT implemented & property file updated

---
+++ src/main/java/org/application/hotelbookingappbe/security/jwt/JwtService.java
@@ -0,0 +1,77 @@
+package org.application.hotelbookingappbe.security.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.application.hotelbookingappbe.security.user.HotelUserDetails;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class JwtService {
+    private static final Logger logger = LoggerFactory.getLogger(JwtService.class);
+
+    @Value("${security.jwt.secret}")
+    private String secret;
+
+    @Value("${security.jwt.expirationTimeMs}")
+    private int expirationTimeMs = 86400000;
+
+    public String generateJwtToken(Authentication authentication) {
+        HotelUserDetails userPrincipal = (HotelUserDetails) authentication.getPrincipal();
+        List<String> roles = userPrincipal.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority).toList();
+        return Jwts.builder()
+                .setSubject(userPrincipal.getUsername())
+                .claim("roles", roles)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTimeMs))
+                .signWith(signingKey(), SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    private SecretKey signingKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String getUsernameFromJwtToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(signingKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public Boolean validateJwtToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(signingKey())
+                    .build()
+                    .parse(token);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        } catch (Exception e) {
+            logger.error("JWT token is invalid: {}", e.getMessage());
+        }
+        return false;
+    }
+}
