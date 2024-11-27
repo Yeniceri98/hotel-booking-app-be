cd2b6443939f0ba8bece68e3bccf5f7d5bc48bf2
Ahmet Sami Yeniçeri <ahmet.sami.yeniceri@ericsson.com>
2024-11-27 13:57:34 +0300

    login endpoint added

--- src/main/java/org/application/hotelbookingappbe/controller/AuthController.java
+++ src/main/java/org/application/hotelbookingappbe/controller/AuthController.java
@@ -2,19 +2,30 @@ package org.application.hotelbookingappbe.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.application.hotelbookingappbe.model.User;
+import org.application.hotelbookingappbe.response.JwtResponse;
+import org.application.hotelbookingappbe.security.jwt.JwtService;
+import org.application.hotelbookingappbe.security.user.HotelUserDetails;
 import org.application.hotelbookingappbe.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     @PostMapping("/register")
     public ResponseEntity<User> registerUser(@RequestBody User user) {
@@ -22,8 +33,16 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody User user) {
-        // Authentication logic will be implemented later with JWT
-        return new ResponseEntity<>("Login successful", HttpStatus.OK);
+    public ResponseEntity<?> login(@RequestBody User user) {
+        Authentication authentication = authenticationManager.authenticate(
+                new org.springframework.security.authentication.UsernamePasswordAuthenticationToken(
+                        user.getEmail(), user.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtService.generateJwtToken(authentication);
+        HotelUserDetails userDetails = (HotelUserDetails) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+        return ResponseEntity.ok(new JwtResponse(userDetails.getId(), userDetails.getEmail(), jwt, roles));
     }
 }
