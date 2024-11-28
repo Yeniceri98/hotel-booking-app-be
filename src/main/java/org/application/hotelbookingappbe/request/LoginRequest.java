5ab2c6ef25c35754420c6e60233651f9368c6bef
Ahmet Sami Yeniçeri <ahmet.sami.yeniceri@ericsson.com>
2024-11-28 14:05:28 +0300

    LoginRequest added

---
+++ src/main/java/org/application/hotelbookingappbe/request/LoginRequest.java
@@ -0,0 +1,13 @@
+package org.application.hotelbookingappbe.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+}
