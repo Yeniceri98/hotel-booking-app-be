f5907a28915eb151f16a051bf683db846ddf55e5
Ahmet Sami Yeniçeri <ahmet.sami.yeniceri@ericsson.com>
2024-12-05 14:32:34 +0300

    Removed unused imports

--- src/main/java/org/application/hotelbookingappbe/model/Role.java
+++ src/main/java/org/application/hotelbookingappbe/model/Role.java
@@ -1,7 +1,6 @@
 package org.application.hotelbookingappbe.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
