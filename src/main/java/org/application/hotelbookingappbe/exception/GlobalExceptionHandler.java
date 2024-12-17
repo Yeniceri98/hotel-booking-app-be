ea397133f628722ceeecf05b72cf91ed7d2326fc
Ahmet Sami Yeniçeri <ahmet.sami.yeniceri@ericsson.com>
2024-12-17 11:22:45 +0300

    Exception for validation processes is added

--- src/main/java/org/application/hotelbookingappbe/exception/GlobalExceptionHandler.java
+++ src/main/java/org/application/hotelbookingappbe/exception/GlobalExceptionHandler.java
@@ -2,14 +2,26 @@ package org.application.hotelbookingappbe.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    // For Validation Errors
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(RoomIsNotFoundException.class)
     public ResponseEntity<ErrorObject> handleRoomIsNotFoundException(RoomIsNotFoundException ex, WebRequest request) {
