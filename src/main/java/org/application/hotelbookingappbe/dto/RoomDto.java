71c02597cb8ed54139543fd79bf297f36c280a11
Ahmet Sami Yeniçeri <ahmet.sami.yeniceri@ericsson.com>
2024-12-18 09:33:51 +0300

    Builder annotation added

--- src/main/java/org/application/hotelbookingappbe/dto/RoomDto.java
+++ src/main/java/org/application/hotelbookingappbe/dto/RoomDto.java
@@ -1,11 +1,13 @@
 package org.application.hotelbookingappbe.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
