30926ac9f5d97d681249e81663bfcb56720ac64a
Ahmet Sami Yeniceri <ahmet.sami.yeniceri@ericsson.com>
2025-03-11 11:19:43 +0300

    Get BCryptPassword for manually created ADMIN role

--- src/main/java/org/application/hotelbookingappbe/HotelBookingAppBeApplication.java
+++ src/main/java/org/application/hotelbookingappbe/HotelBookingAppBeApplication.java
@@ -2,12 +2,14 @@ package org.application.hotelbookingappbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class HotelBookingAppBeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HotelBookingAppBeApplication.class, args);
+        // System.out.println(new BCryptPasswordEncoder().encode("admin123"));     // Used for to get BCRYPT password when manually create ADMIN role
     }
 
 }
